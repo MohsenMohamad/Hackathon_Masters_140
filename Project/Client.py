001c4a7cfec8a7e4308095511c7050a2e467be03
MohsenMohamad <mohsenm@post.bgu.ac.il>
2020-12-24 03:45:45 +0200

    Server now replies with a counter for each input

--- Project/Client.py
+++ Project/Client.py
@@ -1,24 +1,33 @@
 import socket
+from pynput.keyboard import Key,Listener
+import threading
+
+
+host = socket.gethostname()  # as both code is running on same pc
+port = 5000  # socket server port number
+client_socket = socket.socket()  # instantiate
+client_socket.connect((host, port))  # connect to the server
+
+
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
-
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
-
-    message = input(" -> ")  # take input
 
     while True:
-        client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
+        print('Counter : ' + data)  # show in terminal
 
     client_socket.close()  # close the connection
 
 
+def on_release(key):
+    client_socket.send(str(key).encode())    # send message
+
+
 if __name__ == '__main__':
-    client_program()
+    game_thread = threading.Thread(target=client_program)
+    game_thread.start()
+    with Listener(on_release=on_release) as listener:
+        listener.join()
+        game_thread.join()
+
+
--- Project/Server.py
+++ Project/Server.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def server_program():
     # get the hostname
     host = socket.gethostname()
@@ -13,15 +14,16 @@ def server_program():
     server_socket.listen(2)
     conn, address = server_socket.accept()  # accept new connection
     print("Connection from: " + str(address))
+    counter = 0
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
         if not data:
             # if data is not received break
             break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        print(data)
+        counter = counter+1
+        conn.send(str(counter).encode())  # send data to the client
 
     conn.close()  # close the connection
 
