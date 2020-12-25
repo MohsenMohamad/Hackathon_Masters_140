6f7a8e608daa6847e0749d304ddeb9fd479837a8
MohsenMohamad <mohsenm@post.bgu.ac.il>
2020-12-25 08:13:22 +0200

    started synchronizing between broadcast and game

--- Project/Server.py
+++ Project/Server.py
@@ -8,24 +8,51 @@ server_port = 2050  # initiate port no above 1024
 broadcast_port = 13117  # this should be the port in the end when we test it
 teams = []  # do not forget to clear it after every match
 game_result = "\ngame result"
+synchro_object = threading.Condition()
 
 
 def server_broadcast():
 
+    # broadcast socket
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Enable broadcasting mode
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    server.settimeout(0.2)
+    message = UDPMessage.send_offer(server_port)
+
+    # server accept socket
+
+    # get the hostname
+    host = socket.gethostname()
+
+    server_socket = socket.socket()  # get instance
+
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, server_port))  # bind host address and port together
+    server_socket.setblocking(False)
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen()
+
     while True:
-        server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # Enable broadcasting mode
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        # Set a timeout so the socket does not block
-        # indefinitely when trying to receive data.
-        server.settimeout(0.2)
-        message = UDPMessage.send_offer(server_port)
-
-        while len(teams) < 4:
+        stop_broadcast = time.time() + 10
+        while time.time() < stop_broadcast:
             server.sendto(message, ('<broadcast>', broadcast_port))
+            try:
+                conn, address = server_socket.accept()  # accept new connection
+                teams.append(conn.recv(1024).decode())  # add the team's name
+                print("Connection from: " + str(address))
+                client_thread = threading.Thread(target=start_game, args=(conn,))
+                client_thread.start()
+            except:
+                print(end='\r')
             time.sleep(1)
-
+        with synchro_object:
+            synchro_object.wait()
         teams.clear()
 
 
@@ -38,11 +65,13 @@ def server_accepting():
     server_socket.bind((host, server_port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-    server_socket.listen(4)
+    server_socket.listen()
 
     while True:
         conn, address = server_socket.accept()  # accept new connection
-        teams.append(conn.recv(1024).decode())
+        print(server_socket.getsockname())
+        print(conn.getsockname())
+        teams.append(conn.recv(1024).decode())  # add the team's name
         print(teams)
         print("Connection from: " + str(address))
         client_thread = threading.Thread(target=start_game, args=(conn,))
@@ -68,10 +97,13 @@ def start_game(connection_socket):
             print(end='\r')
     connection_socket.send(game_result.encode())
     connection_socket.close()  # close the connection
+    with synchro_object:
+        synchro_object.notifyAll()
 
 
 if __name__ == '__main__':
     print("Server started,listening on IP address : " + socket.gethostbyname(socket.gethostname()))
-    broadcast_thread = threading.Thread(target=server_broadcast)
-    broadcast_thread.start()
-    server_accepting()
+    server_broadcast()
+#    broadcast_thread = threading.Thread(target=server_broadcast)
+#    broadcast_thread.start()
+#    server_accepting()
