48e0c94dbcd4cd159fdb6e43b31e889b89198fbe
MohsenMohamad <mohsenm@post.bgu.ac.il>
2020-12-26 14:27:04 +0200

    working on asyncio client

--- Project/ClientN.py
+++ Project/ClientN.py
@@ -1,16 +1,16 @@
-import socket
-from pynput.keyboard import Key, Listener
-import threading
-import UDPMessage
+import asyncio
 import struct
+import socket
+from pynput.keyboard import Listener
 
 
-tcp_socket = socket.socket()
 broadcast_port = 13117
 team_name = "Instinct"
+reader = 0
+writer = 0
 
 
-def client_listen():
+async def client_listen():
 
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,45 +18,40 @@ def client_listen():
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     client.bind(("", broadcast_port))
     while True:
-        global tcp_socket
-        tcp_socket = socket.socket()
         data, addr = client.recvfrom(1024)
         unpacked_data = struct.unpack('QQQ', data)
-#       print(hex(unpacked_data[0]))
         host_name = addr[0]  # (eth1, 172.1.0/24) is for development , (eth2, 172.99.0/24) is to test your work
         print("Received offer from " + str(host_name) + ", attempting to connect...")
         invitation_port = unpacked_data[2]    # socket server port number
-        client_connect(host_name, invitation_port)
+        await client_connect(host_name, invitation_port)
 
 
-def client_connect(hostname, port):
-
-    tcp_socket.connect((hostname, port))  # connect to the server
-    tcp_socket.send((team_name + "\n").encode())
+async def client_connect(hostname, port):
+    global reader, writer
+    reader, writer = await asyncio.open_connection(hostname, port)  # connect to the server
+    writer.write(team_name.encode())
     with Listener(on_release=on_release) as listener:
-        client_game()
+        await client_game()
 
 
-def client_game():
+async def client_game():
 
     #   the server should send a tcp packet to stop the game at the end
     while True:
-        data = tcp_socket.recv(1024).decode()  # receive response
+        data1 = await reader.read(1024)
+        data = data1.decode()  # receive response
         if not data:
             break
     #    print('Counter : ' + data)  # show in terminal
         print(data)  # show in terminal
-    tcp_socket.close()  # close the connection
+    writer.close()  # close the connection
     print("Server disconnected, listening for offer requests...")
 
 
-def on_release(key):
-    tcp_socket.send(str(key).encode())    # send message
+async def on_release(key):
+    writer.write(str(key).encode())    # send message
 
 
 if __name__ == '__main__':
-    print("Client started, listening for offer requests...")
-    client_listen()
-
-
-
+    asyncio.run(client_listen())
+    # asyncio.run(tcp_echo_client('Hello World!'))
---
+++ Project/ServerN.py
@@ -0,0 +1,30 @@
+import asyncio
+import socket
+
+
+async def handle_request(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close()
+
+
+async def main():
+    server = await asyncio.start_server(handle_request, socket.gethostbyname(socket.gethostname()), 2050)
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
