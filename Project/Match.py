8b59587a6a67c14c8c377d25a8f8874dc9e2d1e9
TariqGanem <tariqg@post.bgu.ac.il>
2020-12-28 01:09:41 +0200

    Stats (half)Impl and printing issues

--- Project/Match.py
+++ Project/Match.py
@@ -21,27 +21,24 @@ class Match:
 
     def start_game_msg(self):
         game_message = "\nWelcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n"
-        msg = game_message + concatenate_list_data(self.group1)
+        msg = game_message + concatenate_list_data(self.group1, 0)
         msg += "Group 2:\n==\n"
-        msg += concatenate_list_data(self.group2)
+        msg += concatenate_list_data(self.group2, 0)
         msg += "Start pressing keys on your keyboard as fast as you can!!\n"
         return msg
 
     def print_result(self):
-        str1 = "Group 1 typed in " + str(self.group1_result) + " characters. "
-        str2 = "Group 2 typed in " + str(self.group2_result) + " characters."
-        print("\nGame Over!\n" + str1 + str2)
+        str1 = "\u001b[36mGroup 1 typed in " + str(self.group1_result) + " characters. "
+        str2 = "Group 2 typed in " + str(self.group2_result) + " characters.\033[0m"
+        print("\n\u001b[31mGame Over!\033[0m\n" + str1 + str2)
         if self.group1_result > self.group2_result:
-            winners = concatenate_list_data(self.group1) + "Game over, sending out offer requests..."
-            print(
-                "\033[1;3;32mGroup 1 wins!\033[0m\n\n\u001b[36mCongratulations to the winners:\033[0m\n==\n" + winners)
+            winners = concatenate_list_data(self.group1, 1) + "Game over, sending out offer requests..."
+            print("\033[1;3;32mGroup 1 wins!\033[0m\n\n\u001b[36mCongratulations to the winners:\n==\033[0m\n" + winners)
         elif self.group2_result > self.group1_result:
-            winners = concatenate_list_data(self.group2) + "Game over, sending out offer requests..."
-            print(
-                "\033[1;3;32mGroup 2 wins!\033[0m\n\n\u001b[36mCongratulations to the winners:\033[0m\n==\n" + winners)
+            winners = concatenate_list_data(self.group2, 1) + "Game over, sending out offer requests..."
+            print("\033[1;3;32mGroup 2 wins!\033[0m\n\n\u001b[36mCongratulations to the winners:\n==\033[0m\n" + winners)
         else:
-            print(
-                "\033[0;31mDraw!\033[0m\nNone of the groups won the game!\n" + "Game over, sending out offer requests...")
+            print("\033[0;31mDraw!\033[0m\n\033[1;3;32mNone of the groups won the game!\033[0m\n" + "Game over, sending out offer requests...")
     #    group1_result = 0
     #    group2_result = 0
 
@@ -62,9 +59,13 @@ class Match:
     # the sockets ( we may have to change that so we can safely call print_result )
 
 
-def concatenate_list_data(lst):
+def concatenate_list_data(lst, color):
     result = ""
     for element in lst:
+        if color == 1:
+            result += "\033[1;3;32m"
         result += element
+        if color == 1:
+            result += "\033[0m"
         result += "\n"
     return result
--- Project/Stats.py
+++ Project/Stats.py
@@ -1,3 +1,35 @@
-#Top 3 who have scored in a game
-#Most winning games for certain user
-#
+class Data:
+
+    def __init__(self):
+        self.groups = {}  # group_name, [all teams]
+        self.statistics1 = {}  # group_name, scores
+        self.statistics2 = {}  # group_name, number_of_games_won
+
+    def top_three_winners(self):  # prints top 3 groups who won highest number of matches.
+        dct = self.statistics2
+        group1 = max(dct, key=dct.get)
+        dct.pop(group1)
+        group2 = max(dct, key=dct.get)
+        dct.pop(group2)
+        group3 = max(dct, key=dct.get)
+        dct.pop(group3)
+        print("\u001b[36mHere are top 3 groups winners, who got most winning games!\033[0m")
+        print("Group1:\n==")
+        self.print_teams(group1)
+        print("Group2:\n==")
+        self.print_teams(group1)
+        print("Group3:\n==")
+        self.print_teams(group1)
+        print("\033[1;3;32mCongratulations, you are the best!\033[0m\n")
+
+    def highest_score(self):  # prints the group who has highest score in a single match.
+        dct = self.statistics1
+        winner = max(dct, key=dct.get)
+        print("\u001b[36mHere is the group who scored the most in one match!\033[0m\n")
+        print("Group members:\n==")
+        self.print_teams(winner)
+
+    def print_teams(self, group_name):
+        lst = self.groups[group_name]
+        for item in lst:
+            print("\033[1;3;32m" + item + "\033[0m\n")
