f5e7b3c6d0e7558803b158f83cf8ac29eefbbe48
MohsenMohamad <mohsenm@post.bgu.ac.il>
2020-12-27 18:28:40 +0200

    Added helper functions for server socket creations

--- Project/Server.py
+++ Project/Server.py
@@ -11,23 +11,15 @@ match = Match.Match()
 
 def server_broadcast(server_port, broadcast_port):
     global match
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)   # broadcast socket
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # Enable broadcasting mode
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    server.settimeout(0.2)   # Set a timeout so the socket does not block indefinitely when trying to receive data.
+    broadcast_socket = create_broadcast_socket()
     message = UDPMessage.send_offer(server_port)
-
-    host = socket.gethostname()  # get the hostname
-    server_socket = socket.socket()  # get instance
-    server_socket.bind((host, server_port))  # bind host address and port together
-    server_socket.setblocking(False)    # set socket to non-blocking mode
-    server_socket.listen()      # configure how many client the server can listen simultaneously
+    server_socket = create_server_socket(server_port)
 
     while True:
         match = Match.Match()
         stop_broadcast = time.time() + 10
         while time.time() < stop_broadcast:
-            server.sendto(message, ('<broadcast>', broadcast_port))
+            broadcast_socket.sendto(message, ('<broadcast>', broadcast_port))
             try:
                 conn, address = server_socket.accept()  # accept new connection
                 conn.setblocking(True)
@@ -39,15 +31,29 @@ def server_broadcast(server_port, broadcast_port):
                     match.add_team_to_group1(team_name, client_thread)
                 else:
                     match.add_team_to_group2(team_name, client_thread)
-
             except:
                 print(end='\r')
             time.sleep(1)
         match.run_client_threads()
         time.sleep(10)
         match.print_result()
-    #    group1.clear()
-    #    group2.clear()
+
+
+def create_broadcast_socket():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)   # broadcast socket
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # Enable broadcasting mode
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    udp_socket.settimeout(0.2)   # Set a timeout so the socket does not block indefinitely when trying to receive data.
+    return udp_socket
+
+
+def create_server_socket(server_port):
+    host = socket.gethostname()  # get the hostname
+    server_socket = socket.socket()  # get instance
+    server_socket.bind((host, server_port))  # bind host address and port together
+    server_socket.setblocking(False)  # set socket to non-blocking mode
+    server_socket.listen()  # configure how many client the server can listen simultaneously
+    return server_socket
 
 
 if __name__ == '__main__':
