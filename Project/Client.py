f91e675787bf5a9507e4d94ffb4793fd98140f8a
MohsenMohamad <mohsenm@post.bgu.ac.il>
2020-12-29 18:52:22 +0200

    replaced pynput with keyboard

--- Project/Client.py
+++ Project/Client.py
@@ -1,13 +1,15 @@
 import socket
-from pynput.keyboard import Listener
+import keyboard
 import UDPMessage
+import threading
 
 tcp_socket = socket.socket()
 team_name = "Masters\n"
+stop_keyboard = False
 
 
 def client_listen(broadcast_port):
-
+    global stop_keyboard
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Enable broadcasting mode
@@ -22,6 +24,7 @@ def client_listen(broadcast_port):
         print("Received offer from " + str(host_name) + ", attempting to connect...")
         client_connect(host_name, invitation_port)
         clear_previous_invitations(client)
+        stop_keyboard = False
         print("\nServer disconnected, listening for offer requests...")
 
 
@@ -41,23 +44,27 @@ def client_connect(hostname, port):
         tcp_socket.close()
         return
     tcp_socket.settimeout(socket.getdefaulttimeout())
-    client_game()
+    global stop_keyboard
+    keyboard_thread = threading.Thread(target=listen)
+    client_game(keyboard_thread)
 
 
-def client_game():
+def client_game(key_thread):
+    global stop_keyboard
     try:
         welcome_message = tcp_socket.recv(1024).decode()
         print(welcome_message)
-        with Listener(on_release=on_release) as listener:
-            #   the server should send a tcp packet to stop the game at the end
-            while True:
-                data = tcp_socket.recv(1024).decode()  # receive response
-                if not data:
-                    break
-                print(data)  # show in terminal
-            tcp_socket.close()  # close the connection
+        key_thread.start()
+        while True:
+            data = tcp_socket.recv(1024).decode()  # receive response
+            if not data:
+                break
+            print(data)  # show in terminal
+        stop_keyboard = True
+        tcp_socket.close()  # close the connection
     except socket.error as err:
         print("Error during the game : "+str(err))
+        stop_keyboard = True
         tcp_socket.close()
         return
 
@@ -77,6 +84,19 @@ def clear_previous_invitations(client):
         except socket.error:
             client.setblocking(True)
             return
+
+
+def on_key_release(key):
+    try:
+        tcp_socket.sendall(str(key).encode())  # send message
+    except socket.error as err:
+        print("Could not send the keyboard input to server : " + str(err))
+
+
+def listen():
+    keyboard.on_release(callback=on_key_release)
+    if stop_keyboard:
+        return
 
 
 if __name__ == '__main__':
