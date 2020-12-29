c13c7df19e62483f56894320b5e7a94fa2df12c2
MohsenMohamad <mohsenm@post.bgu.ac.il>
2020-12-29 09:52:47 +0200

    Client error handling + fixed invitation bug

--- Project/Client.py
+++ Project/Client.py
@@ -20,36 +20,69 @@ def client_listen(broadcast_port):
         global tcp_socket
         tcp_socket = socket.socket()
         data, addr = client.recvfrom(1024)
+        print("here")
         unpacked_data = struct.unpack('QQQ', data)
         host_name = addr[0]  # (eth1, 172.1.0/24) is for development , (eth2, 172.99.0/24) is to test your work
         print("Received offer from " + str(host_name) + ", attempting to connect...")
         invitation_port = unpacked_data[2]    # socket server port number
         client_connect(host_name, invitation_port)
+        clear_previous_invitations(client)
+        print("\nServer disconnected, listening for offer requests...")
 
 
 def client_connect(hostname, port):
 
-    tcp_socket.connect((hostname, port))  # connect to the server
-    tcp_socket.sendall((team_name + "\n").encode())
+    tcp_socket.settimeout(10)
+    try:
+        tcp_socket.connect((hostname, port))  # connect to the server
+    except socket.error as err:
+        print("Error while trying to connect to server : "+str(err))
+        return
+
+    try:
+        tcp_socket.sendall((team_name + "\n").encode())
+    except socket.error as err:
+        print("Error at sending the team name : "+str(err))
+        tcp_socket.close()
+        return
+    tcp_socket.settimeout(socket.getdefaulttimeout())
     client_game()
 
 
 def client_game():
-    welcome_message = tcp_socket.recv(1024).decode()
-    print(welcome_message)
-    with Listener(on_release=on_release) as listener:
-        #   the server should send a tcp packet to stop the game at the end
-        while True:
-            data = tcp_socket.recv(1024).decode()  # receive response
-            if not data:
-                break
-            print(data)  # show in terminal
-        tcp_socket.close()  # close the connection
-        print("\nServer disconnected, listening for offer requests...")
+    try:
+        welcome_message = tcp_socket.recv(1024).decode()
+        print(welcome_message)
+        with Listener(on_release=on_release) as listener:
+            #   the server should send a tcp packet to stop the game at the end
+            while True:
+                data = tcp_socket.recv(1024).decode()  # receive response
+                if not data:
+                    break
+                print(data)  # show in terminal
+            tcp_socket.close()  # close the connection
+            print("\nServer disconnected, listening for offer requests...")
+    except socket.error as err:
+        print("Error during the game : "+str(err))
+        tcp_socket.close()
+        return
 
 
 def on_release(key):
-    tcp_socket.sendall(str(key).encode())    # send message
+    try:
+        tcp_socket.sendall(str(key).encode())    # send message
+    except socket.error as err:
+        print("Could not send the keyboard input to server : "+str(err))
+
+
+def clear_previous_invitations(client):
+    client.setblocking(False)
+    while True:
+        try:
+            client.recv(1024)
+        except socket.error:
+            client.setblocking(True)
+            return
 
 
 if __name__ == '__main__':
